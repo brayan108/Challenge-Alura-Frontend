862489983850204ab777bce26f43a69e3b3b605a
brayan108 <100053102+brayan108@users.noreply.github.com>
2024-11-20 15:10:48 -0500

    Update limpiar.js

--- js/limpiar.js
+++ js/limpiar.js
@@ -2,7 +2,10 @@
 const botonLimpiar = document.getElementById('limpiar');
 
 
-botonLimpiar.addEventListener('click', () => {
+botonLimpiar.addEventListener('click', (e) => {
+
+    e.preventDefault();
+    
     document.querySelector('[data-nombre]').value = '';
     document.querySelector('[data-precio]').value = '';
     document.querySelector('[data-imagen]').value = '';
